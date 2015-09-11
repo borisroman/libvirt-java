cf78b9d8933dbc04a4e9515c95faa71b047a7aaf
Boris Schrijver <boris@pcextreme.nl>
2015-09-11 10:20:39 +0200

    Moved virDomainSnapshotCreateFlags to DomainSnapshot.java. Added VIR_DOMAIN_SNAPSHOT_CREATE_LIVE to virDomainSnapshotCreateFlags.

--- src/main/java/org/libvirt/Domain.java
+++ src/main/java/org/libvirt/Domain.java
@@ -44,28 +44,6 @@ public class Domain {
         public static final int BYTES = 1;
     }
 
-    static final class CreateFlags {
-        static final int VIR_DOMAIN_NONE = 0;
-        static final int VIR_DOMAIN_SNAPSHOT_CREATE_REDEFINE    = (1 << 0); /* Restore or alter
-                                                                               metadata */
-        static final int VIR_DOMAIN_SNAPSHOT_CREATE_CURRENT     = (1 << 1); /* With redefine, make
-                                                                               snapshot current */
-        static final int VIR_DOMAIN_SNAPSHOT_CREATE_NO_METADATA = (1 << 2); /* Make snapshot without
-                                                                               remembering it */
-        static final int VIR_DOMAIN_SNAPSHOT_CREATE_HALT        = (1 << 3); /* Stop running guest
-                                                                               after snapshot */
-        static final int VIR_DOMAIN_SNAPSHOT_CREATE_DISK_ONLY   = (1 << 4); /* disk snapshot, not
-                                                                               system checkpoint */
-        static final int VIR_DOMAIN_SNAPSHOT_CREATE_REUSE_EXT   = (1 << 5); /* reuse any existing
-                                                                               external files */
-        static final int VIR_DOMAIN_SNAPSHOT_CREATE_QUIESCE     = (1 << 6); /* use guest agent to
-                                                                               quiesce all mounted
-                                                                               file systems within
-                                                                               the domain */
-        static final int VIR_DOMAIN_SNAPSHOT_CREATE_ATOMIC      = (1 << 7); /* atomically avoid
-                                                                               partial changes */
-    }
-
     static final class MigrateFlags {
         static final int VIR_MIGRATE_LIVE              = (1 << 0); /* live migration */
         static final int VIR_MIGRATE_PEER2PEER         = (1 << 1); /* direct source -> dest host control channel */
--- src/main/java/org/libvirt/DomainSnapshot.java
+++ src/main/java/org/libvirt/DomainSnapshot.java
@@ -8,6 +8,30 @@ import com.sun.jna.Pointer;
 
 public class DomainSnapshot {
 
+    static final class virDomainSnapshotCreateFlags {
+        static final int VIR_DOMAIN_SNAPSHOT_CREATE_REDEFINE    = (1 << 0); /* Restore or alter
+                                                                               metadata */
+        static final int VIR_DOMAIN_SNAPSHOT_CREATE_CURRENT     = (1 << 1); /* With redefine, make
+                                                                               snapshot current */
+        static final int VIR_DOMAIN_SNAPSHOT_CREATE_NO_METADATA = (1 << 2); /* Make snapshot without
+                                                                               remembering it */
+        static final int VIR_DOMAIN_SNAPSHOT_CREATE_HALT        = (1 << 3); /* Stop running guest
+                                                                               after snapshot */
+        static final int VIR_DOMAIN_SNAPSHOT_CREATE_DISK_ONLY   = (1 << 4); /* disk snapshot, not
+                                                                               system checkpoint */
+        static final int VIR_DOMAIN_SNAPSHOT_CREATE_REUSE_EXT   = (1 << 5); /* reuse any existing
+                                                                               external files */
+        static final int VIR_DOMAIN_SNAPSHOT_CREATE_QUIESCE     = (1 << 6); /* use guest agent to
+                                                                               quiesce all mounted
+                                                                               file systems within
+                                                                               the domain */
+        static final int VIR_DOMAIN_SNAPSHOT_CREATE_ATOMIC      = (1 << 7); /* atomically avoid
+                                                                               partial changes */
+        static final int VIR_DOMAIN_SNAPSHOT_CREATE_LIVE        = (1 << 8); /* create the snapshot
+                                                                               while the guest is
+                                                                               running */
+    }
+
     /**
      * the native virDomainSnapshotPtr.
      */
