4e4768ac0265d405ded23a0c08f9c370fc79b788
Boris Schrijver <boris@pcextreme.nl>
2015-09-11 09:50:59 +0200

    Added VIR_DOMAIN_PMSUSPENDED to DomainInfo.java.

--- src/main/java/org/libvirt/DomainInfo.java
+++ src/main/java/org/libvirt/DomainInfo.java
@@ -41,7 +41,12 @@ public class DomainInfo {
         /**
          * the domain is crashed
          */
-        VIR_DOMAIN_CRASHED
+        VIR_DOMAIN_CRASHED,
+        /**
+         * the domain is suspended by guest power management
+         */
+        VIR_DOMAIN_PMSUSPENDED
+        
     }
 
     /**
