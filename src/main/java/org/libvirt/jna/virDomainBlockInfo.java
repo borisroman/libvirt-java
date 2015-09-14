74dbb99d6ac103126bfaff806ed58cad6d6d6479
Boris Schrijver <boris@pcextreme.nl>
2015-09-14 11:37:01 +0200

    Refactored JavaDoc for virDomainBlockInfo

--- src/main/java/org/libvirt/jna/virDomainBlockInfo.java
+++ src/main/java/org/libvirt/jna/virDomainBlockInfo.java
@@ -6,8 +6,19 @@ import java.util.List;
 import com.sun.jna.Structure;
 
 public class virDomainBlockInfo extends Structure {
+    /**
+     * Logical size in bytes of the image (how much storage the guest will see)
+     */
     public long capacity;
+    /**
+     * Host storage in bytes occupied by the image (such as highest
+     * allocated extent if there are no holes, similar to 'du')
+     */
     public long allocation;
+    /**
+     * Host physical size in bytes of the image container (last
+     * offset, similar to 'ls')
+     */
     public long physical;
 
     private static final List<String> fields = Arrays.asList(
