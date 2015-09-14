f95dc4f2e0c0bad0126a60fb1120b2cdbc760eec
Boris Schrijver <boris@pcextreme.nl>
2015-09-14 11:29:21 +0200

    Refactored JavaDoc for virDomainBlockStats

--- src/main/java/org/libvirt/jna/virDomainBlockStats.java
+++ src/main/java/org/libvirt/jna/virDomainBlockStats.java
@@ -9,16 +9,26 @@ import com.sun.jna.Structure;
  * JNA mapping for the virDomainBlockStats structure
  */
 public class virDomainBlockStats extends Structure {
-    public long rd_req; // this is a long long in the code, so a long mapping is
-    // correct
-    public long rd_bytes;// this is a long long in the code, so a long mapping
-    // is correct
-    public long wr_req; // this is a long long in the code, so a long mapping is
-    // correct
-    public long wr_bytes;// this is a long long in the code, so a long mapping
-    // is correct
-    public long errs; // this is a long long in the code, so a long mapping is
-    // correct
+    /**
+     * This is a long long in the code, so a long mapping is correct.
+     */
+    public long rd_req;
+    /**
+     * This is a long long in the code, so a long mapping is correct
+     */
+    public long rd_bytes;
+    /**
+     * This is a long long in the code, so a long mapping is correct
+     */
+    public long wr_req;
+    /**
+     * This is a long long in the code, so a long mapping is correct
+     */
+    public long wr_bytes;
+    /**
+     * This is a long long in the code, so a long mapping is correct
+     */
+    public long errs;
 
     private static final List<String> fields = Arrays.asList(
             "rd_req", "rd_bytes", "wr_req", "wr_bytes", "errs");
