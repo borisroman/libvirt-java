8ee517c03da9efb4dd6e4a2ceeeac69f2536c8ae
Boris Schrijver <boris@pcextreme.nl>
2015-09-14 11:48:06 +0200

    Added virtDomainIOThreadInfo structure.

---
+++ src/main/java/org/libvirt/jna/virDomainIOThreadInfo.java
@@ -0,0 +1,33 @@
+package org.libvirt.jna;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.sun.jna.Structure;
+
+/**
+ * JNA mapping for the virDomainInterfaceStats structure
+ */
+public class virDomainIOThreadInfo extends Structure {
+    /**
+     * IOThread ID
+     */
+    int iothread_id;
+    /**
+     * CPU map for thread. A pointer to an
+    /* array of real CPUs (in 8-bit bytes)
+     */
+    byte[] cpumap;
+    /**
+     * cpumap size
+     */
+    int cpumaplen;
+
+    private static final List<String> fields = Arrays.asList(
+            "iothread_id", "cpumap", "cpumaplen");
+
+    @Override
+    protected List<String> getFieldOrder() {
+        return fields;
+    }
+}
